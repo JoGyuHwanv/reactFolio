bd45187d8109993e877c8be8a158631bae585a58
조규환 <rbghksdlsms@gmail.com>
2024-10-14 16:27:31 +0900

    홈 프로젝트 페이지 내용 추가

--- src/assets/data/ProjectData.js
+++ src/assets/data/ProjectData.js
@@ -3,20 +3,20 @@ const projectData = {
         {
             name: "AI기반 스마트 종합정보 포털시스템 구축사업",
             period: "2021.08.05 - 2022.01.31",
-            description: "테스트입니다.테스트입니다.테스트입니다.테스트입니다.테스트입니다. 테스트입니다.테스트입니다.테스트입니다.테스트입니다.테스트입니다.테스트입니다.테스트입니다.테스트입니다.테스트입니다.테스트입니다.테스트입니다.테스트입니다.테스트입니다.테스트입니다.테스트입니다.",
+            description: "한국해양교통안전공단 프로젝트입니다. 페이지 테스트간 오류수정 및 개발작업을 하였습니다. 프로젝트 진행중에 투입되어 약 6개월간 진행하였습니다.",
             stack: ["Java", "Javascript", "Html", "Database"]
         },
         {    
             name: "SGIS-행정통계시각화지도",
             period: "2022.04.01 - 2023.03.31",
-            description: "테스트입니다.테스트입니다.테스트입니다.테스트입니다.테스트입니다.",
-            stack: ["Java", "Javascript", "ES6", "Html", "Oracle", "API", "Database"]
+            description: "SGIS 통계지리정보서비스 프로젝트입니다. 통계청 API와 Highcharts를 이용해 데이터 시각화작업을 하였습니다. 기간은 약 1년정도 소요되었습니다.",
+            stack: ["Java", "Javascript", "Html", "API", "Database"]
         },
         {    
             name: "차세대 디지털 고용서비스 플랫폼 고용24 구축",
             period: "2023.04.10 - 2023.11.03",
-            description: "테스트입니다.테스트입니다.테스트입니다.테스트입니다.테스트입니다.",
-            stack: ["Java", "Javascript", "Html", "Database"]
+            description: "한국고용정보원의 워크넷과 고용24 통합 프로젝트입니다. 워크넷은 서비스가 종료되고 고용24로 통합되었고 약 100개정도의 페이지를 작업하였습니다. ",
+            stack: ["Java", "Javascript", "Html", "ES6", "Database"]
         }
     ]
 }
